c6edd900b59553a2412ab9af3f27cbf74809b912
Honghao Zhang <zhh358@gmail.com>
2014-06-09 20:10:15 -0400

    initial commit

---
+++ Tiva/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tiva
+//
+//  Created by Zhang Honghao on 6/9/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZHHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZHHAppDelegate class]));
+    }
+}
---
+++ TivaTests/TivaTests.m
@@ -0,0 +1,34 @@
+//
+//  TivaTests.m
+//  TivaTests
+//
+//  Created by Zhang Honghao on 6/9/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TivaTests : XCTestCase
+
+@end
+
+@implementation TivaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
