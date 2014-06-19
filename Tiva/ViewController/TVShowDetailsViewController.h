0b840b37ecd60dd76661f161df395391e24276fa
Kanishk Prasad <kanishk@v1020-wn-166-101.campus-dynamic.uwaterloo.ca>
2014-06-19 15:14:02 -0400

    Added deatils file

---
+++ Tiva/ViewController/TVShowDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TVShowDetailsViewController.h
+//  Tiva
+//
+//  Created by Kanishk Prasad on 6/19/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVShowDetailsViewController : UIViewController
+
+@end
---
+++ Tiva/ViewController/TVShowDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  TVShowDetailsViewController.m
+//  Tiva
+//
+//  Created by Kanishk Prasad on 6/19/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import "TVShowDetailsViewController.h"
+
+@interface TVShowDetailsViewController ()
+
+@end
+
+@implementation TVShowDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
