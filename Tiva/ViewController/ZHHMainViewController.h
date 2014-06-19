8a12c33dbed7b861f901566c56063eada38256c2
Honghao Zhang <zhh358@gmail.com>
2014-06-19 15:07:39 -0400

    iPad

---
+++ Tiva/ViewController/ZHHMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHHMainViewController.h
+//  Tiva
+//
+//  Created by Zhang Honghao on 6/17/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHHMainViewController : UIViewController
+
+@end
---
+++ Tiva/ViewController/ZHHMainViewController.m
@@ -0,0 +1,39 @@
+//
+//  ZHHMainViewController.m
+//  Tiva
+//
+//  Created by Zhang Honghao on 6/17/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import "ZHHMainViewController.h"
+
+@interface ZHHMainViewController ()
+
+@end
+
+@implementation ZHHMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
