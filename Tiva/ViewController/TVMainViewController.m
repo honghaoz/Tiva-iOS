246266c11e1a63049d5f6c4ba1540880a55d31ee
Honghao Zhang <zhh358@gmail.com>
2014-06-21 20:04:48 -0400

    Add today tableView

--- Tiva/ViewController/TVMainViewController.m
+++ Tiva/ViewController/TVMainViewController.m
@@ -13,12 +13,20 @@
 #import "UIImageView+AFNetworking.h"
 #import <NSString+VTContainsSubstring.h>
 
-#define POSTER_ASPECT_RATIO 680 / 1000
+#define POSTER_ASPECT_RATIO (680 / 1000.0)
 #define EMPTY_POSTER_URL_STRING @"http://slurm.trakt.us/images/poster-dark"
+#define BANNER_ASPECT_RATIO (758 / 140.0)
+#define EMPTY_BANNER_URL_STRING @"http://slurm.trakt.us/images/banner"
 
-@interface TVMainViewController () <iCarouselDataSource, iCarouselDelegate> {
-    iCarousel *_mainCarouselView;
+
+@interface TVMainViewController () <iCarouselDataSource, iCarouselDelegate, UITableViewDataSource, UITableViewDelegate> {
     TVShowStore *_sharedShowStore;
+    iCarousel *_carouselView;
+    
+    UIColor *_fontColor;
+    
+    UILabel *_todayLabel;
+    UITableView *_todayTableView;
 }
 
 @end
@@ -38,25 +46,57 @@
     LogMethod;
     self.view = [[UIView alloc] init];
     CGRect mainScreen = [UIScreen mainScreen].bounds;
-    NSLog(@"%@", NSStringFromCGRect(mainScreen));
-    self.view.backgroundColor = [UIColor lightGrayColor];
+    Swap(&mainScreen.size.height, &mainScreen.size.width);
+    self.view.backgroundColor = [UIColor blackColor];
+    // init
+    _fontColor = [UIColor colorWithWhite:1 alpha:0.9];
     
+    // Carousel View
     CGFloat mainCarouselViewX = 0;
     CGFloat mainCarouselViewY = 0;
-    CGFloat mainCarouselViewWidth = mainScreen.size.height;
-    CGFloat mainCarouselViewHeight = mainScreen.size.width * 1 / 2;
+    CGFloat mainCarouselViewWidth = mainScreen.size.width;
+    CGFloat mainCarouselViewHeight = mainScreen.size.height * 0.53;
     CGRect mainCarouselViewRect = CGRectMake(mainCarouselViewX, mainCarouselViewY, mainCarouselViewWidth, mainCarouselViewHeight);
-    _mainCarouselView = [[iCarousel alloc] initWithFrame:mainCarouselViewRect];
-    [_mainCarouselView setType:iCarouselTypeLinear];
-    [_mainCarouselView setDataSource:self];
-    [_mainCarouselView setDelegate:self];
-    [_mainCarouselView setBounces:NO];
+    _carouselView = [[iCarousel alloc] initWithFrame:mainCarouselViewRect];
+    [_carouselView setType:iCarouselTypeLinear];
+    [_carouselView setDataSource:self];
+    [_carouselView setDelegate:self];
+    [_carouselView setBounces:NO];
 //    [_mainCarouselView setDecelerationRate:0.9];
 //    [_mainCarouselView setBounceDistance:0.7];
-    [_mainCarouselView setCenterItemWhenSelected:NO];
-    [_mainCarouselView setContentOffset:CGSizeMake(- (mainCarouselViewWidth - mainCarouselViewHeight * POSTER_ASPECT_RATIO) / 2, 0)];
-    [_mainCarouselView setBackgroundColor:[UIColor blackColor]];
-    [self.view addSubview:_mainCarouselView];
+    [_carouselView setCenterItemWhenSelected:NO];
+    [_carouselView setContentOffset:CGSizeMake(- (mainCarouselViewWidth - mainCarouselViewHeight * POSTER_ASPECT_RATIO) / 2, 0)];
+    [_carouselView setBackgroundColor:[UIColor blackColor]];
+    
+    
+    // Today Label View
+    CGFloat todayLabelX = mainCarouselViewX;
+    CGFloat todayLabelY = mainCarouselViewY + mainCarouselViewHeight;
+    CGFloat todayLabelWidth = mainScreen.size.width * 1/3;
+    CGFloat todayLabelHeight = 40;
+    CGRect todayLabelRect = CGRectMake(todayLabelX, todayLabelY, todayLabelWidth, todayLabelHeight);
+    _todayLabel = [[UILabel alloc] initWithFrame:todayLabelRect];
+    [_todayLabel setText:@"Today"];
+    [_todayLabel setTextAlignment:NSTextAlignmentCenter];
+    [_todayLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:18]];
+    [_todayLabel setBackgroundColor:[UIColor lightGrayColor]];
+    [_todayLabel setTextColor:_fontColor];
+    
+    // Today table view
+    CGFloat todayTableViewX = todayLabelX;
+    CGFloat todayTableViewY = todayLabelY + todayLabelHeight;
+    CGFloat todayTableViewWidth = todayLabelWidth;
+    CGFloat todayTableviewHeight = mainScreen.size.height - mainCarouselViewHeight - todayLabelHeight;
+    CGRect todayTableViewRect = CGRectMake(todayTableViewX, todayTableViewY, todayTableViewWidth, todayTableviewHeight);
+    _todayTableView = [[UITableView alloc] initWithFrame:todayTableViewRect];
+    _todayTableView.dataSource = self;
+    _todayTableView.delegate = self;
+    [_todayTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    
+    [self.view addSubview:_carouselView];
+    [self.view addSubview:_todayLabel];
+    [self.view addSubview:_todayTableView];
+    
 }
 
 - (void)viewDidLoad
@@ -78,8 +118,9 @@
 - (void)showStoreUpdates:(NSNotification *)notification {
     NSLog(@"Received ShowStoreUpdated");
 //    NSInteger updatedIndex = [[notification userInfo][@"ShowIndex"] integerValue];
-    [_mainCarouselView reloadData];
+    [_carouselView reloadData];
 //    [_mainCarouselView reloadItemAtIndex:updatedIndex animated:YES];
+    [_todayTableView reloadData];
 }
 
 
@@ -87,7 +128,6 @@
 
 - (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
 {
-    LogMethod;
     return [_sharedShowStore.shows count];
 }
 
@@ -95,7 +135,7 @@
     UIImageView *theImageView = (UIImageView *)view;
     
     if (theImageView == nil) {
-        theImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _mainCarouselView.bounds.size.height * POSTER_ASPECT_RATIO, _mainCarouselView.bounds.size.height)];
+        theImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, _carouselView.bounds.size.height * POSTER_ASPECT_RATIO, _carouselView.bounds.size.height)];
         [theImageView setContentMode:UIViewContentModeScaleAspectFill];
     }
     [theImageView cancelImageRequestOperation];
@@ -125,6 +165,50 @@
         return 10;
     }
     return value;
+}
+
+#pragma mark - Today table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"%d", [_sharedShowStore.shows count]);
+    return [_sharedShowStore.shows count];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIImageView *bannerImageView = nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TodayShow"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"TodayShow"];
+        CGRect bannerRect = CGRectMake(0, 0, tableView.bounds.size.width, tableView.bounds.size.width / BANNER_ASPECT_RATIO);
+        bannerImageView = [[UIImageView alloc] initWithFrame:bannerRect];
+        [bannerImageView setContentMode:UIViewContentModeScaleAspectFill];
+        [cell.contentView addSubview:bannerImageView];
+    }
+    for (UIView *subView in cell.contentView.subviews) {
+        if ([subView isKindOfClass:[UIImageView class]]) {
+            bannerImageView =  (UIImageView *)subView;
+        }
+    }
+    [bannerImageView cancelImageRequestOperation];
+    [bannerImageView setImage:[self imageWithRect:bannerImageView.bounds.size color:[UIColor lightGrayColor]]];
+    NSString *bannerURLString = [[_sharedShowStore.shows[indexPath.row] bannerURL] absoluteString];
+    if (![bannerURLString vt_containsSubstring:EMPTY_BANNER_URL_STRING]) {
+//        [bannerImageView setImage:[self imageWithRect:bannerImageView.bounds.size color:[UIColor lightGrayColor]]];
+    } else {
+        [bannerImageView setImageWithURL:[_sharedShowStore.shows[indexPath.row] bannerURL]];
+    }
+    
+    return cell;
+}
+
+#pragma mark - Today table view delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return tableView.bounds.size.width / BANNER_ASPECT_RATIO;
 }
 
 #pragma mark - Helper methods
